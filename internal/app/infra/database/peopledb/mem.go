2150fbb5886882789a687188d4824eb9b521580a
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2023-08-14 22:29:39 -0300

    remove unique index from memdb

--- internal/app/infra/database/peopledb/mem.go
+++ internal/app/infra/database/peopledb/mem.go
@@ -117,7 +117,6 @@ func NewMemDb() *MemDb {
 				Indexes: map[string]*memdb.IndexSchema{
 					"id": {
 						Name:    "id",
-						Unique:  true,
 						Indexer: &memdb.StringFieldIndex{Field: "Key"},
 					},
 				},
