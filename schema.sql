181394c8f9262409db601635a718b92a43e01838
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2023-08-15 03:30:52 -0300

    .

--- schema.sql
+++ schema.sql
@@ -1,9 +1,8 @@
 CREATE TABLE
     IF NOT EXISTS public.people (
-        id uuid PRIMARY KEY NOT NULL,
-        nickname varchar(32) NOT NULL,
+        id uuid NOT NULL,
+        nickname varchar(32) PRIMARY KEY NOT NULL,
         "name" varchar(100) NOT NULL,
         birthdate date NULL,
         stack text NULL,
-        CONSTRAINT people_nickname_key UNIQUE (nickname)
     );
