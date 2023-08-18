2a99b19ae80ec57df969be83fbf6e3c719a56948
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2023-08-18 00:41:29 -0300

    time db-search

--- internal/app/infra/database/peopledb/repository.go
+++ internal/app/infra/database/peopledb/repository.go
@@ -83,6 +83,8 @@ func (p *PersonRepository) FindByID(id string) (*people.Person, error) {
 // }
 
 func (p *PersonRepository) Search(term string) ([]people.Person, error) {
+	t := top("db-search")
+	defer t()
 	return p.searchTrigram(term)
 }
 
