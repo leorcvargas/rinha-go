0d5cdf64bbca53d6716b4cb54a782a3f27d63d78
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2023-08-18 11:44:29 -0300

    .

--- scripts/postgres/schema.sql
+++ scripts/postgres/schema.sql
@@ -1,16 +1,8 @@
-CREATE EXTENSION IF NOT EXISTS "unaccent";
-
-CREATE EXTENSION IF NOT EXISTS "pg_trgm";
-
 CREATE TABLE
     IF NOT EXISTS public.people (
         id uuid PRIMARY KEY NOT NULL,
         nickname varchar(32) UNIQUE NOT NULL,
         "name" varchar(100) NOT NULL,
         birthdate date NOT NULL,
-        stack text NULL,
-        search text NOT NULL
+        stack text NULL
     );
-
-CREATE INDEX
-    CONCURRENTLY IF NOT EXISTS idx_people_trigram ON public.people USING gist (search gist_trgm_ops);
