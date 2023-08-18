b1bb3d824ddcd73021336ca9c6097372ad5129a8
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2023-08-17 22:42:33 -0300

    redis concurrent set

--- internal/app/infra/database/peopledb/repository.go
+++ internal/app/infra/database/peopledb/repository.go
@@ -39,7 +39,7 @@ func (p *PersonRepository) Create(person *people.Person) (*people.Person, error)
 		return nil, err
 	}
 
-	p.cache.Set(person.ID, person)
+	go p.cache.Set(person.ID, person)
 
 	return person, nil
 
