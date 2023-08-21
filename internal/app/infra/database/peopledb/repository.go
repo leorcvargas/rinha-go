b208c7974b35bd156729543ad8d57e15afaa50f6
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2023-08-21 15:53:18 -0300

    .

--- internal/app/infra/database/peopledb/repository.go
+++ internal/app/infra/database/peopledb/repository.go
@@ -21,29 +21,49 @@ type PersonRepository struct {
 }
 
 func (p *PersonRepository) Create(person *people.Person) error {
-	_, err := p.db.Exec(
-		context.Background(),
-		InsertPersonQuery,
-		person.ID,
-		person.Nickname,
-		person.Name,
-		person.Birthdate,
-		person.StackStr(),
-		person.SearchStr(),
-	)
+	errorCh := make(chan error, 2)
+	defer close(errorCh)
 
-	if err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
-			return people.ErrNicknameTaken
+	go func() {
+		_, err := p.db.Exec(
+			context.Background(),
+			InsertPersonQuery,
+			person.ID,
+			person.Nickname,
+			person.Name,
+			person.Birthdate,
+			person.StackStr(),
+			person.SearchStr(),
+		)
+
+		if err != nil {
+			var pgErr *pgconn.PgError
+			if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+				errorCh <- people.ErrNicknameTaken
+				return
+			}
+
+			errorCh <- err
+			return
 		}
 
-		return err
-	}
+		errorCh <- nil
+	}()
 
-	err = p.cache.Set(person.ID, person)
-	if err != nil {
-		log.Errorf("Error inserting person in cache: %v", err)
+	go func() {
+		err := p.cache.Set(person.ID, person)
+		if err != nil {
+			log.Errorf("Error inserting person in cache: %v", err)
+		}
+
+		errorCh <- nil
+	}()
+
+	for i := 0; i < 2; i++ {
+		err := <-errorCh
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
