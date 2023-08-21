71ec573a75e04c8a7f1e88de9eaa914cc848d56a
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2023-08-21 17:34:25 -0300

    .

--- internal/app/infra/database/database.go
+++ internal/app/infra/database/database.go
@@ -30,7 +30,6 @@ func NewPostgresDatabase(config *config.Config) *pgxpool.Pool {
 		if err != nil {
 			log.Fatalln("Unable to parse connection url:", err)
 		}
-		poolConfig.MaxConns = 150
 
 		db, err = pgxpool.NewWithConfig(context.Background(), poolConfig)
 		if err != nil {
