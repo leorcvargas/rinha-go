f0a768971a2b8358d721d1033b101d6998a43290
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2023-08-21 21:27:14 -0300

    rollback nginx configs

--- internal/app/infra/database/peopledb/queries.go
+++ internal/app/infra/database/peopledb/queries.go
@@ -5,10 +5,6 @@ const (
 
 	SelectPersonByIDQuery = "SELECT id, nickname, name, birthdate, stack FROM people WHERE id = $1;"
 
-	SearchPeopleFtsQuery = `SELECT id, nickname, name, birthdate, stack FROM people p
-	WHERE p.fts_q @@ plainto_tsquery('people_terms', $1)
-	LIMIT 50;`
-
 	SearchPeopleTrgmQuery = `SELECT id, nickname, name, birthdate, stack FROM people p
 	WHERE p.search LIKE '%' || $1 || '%'
 	LIMIT 50;`
